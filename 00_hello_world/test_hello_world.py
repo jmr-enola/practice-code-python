3a6e08447ccd725adeb50323a134016b3e197f91
jmr-enola <jmr.enola@gmail.com>
2024-01-30 07:38:26 +0800

    refactored test_hello_worl.py to pass pylint

--- 00_hello_world/test_hello_world.py
+++ 00_hello_world/test_hello_world.py
@@ -1,14 +1,18 @@
+"""Test cases for hello_world.py
+"""
+
 from unittest import TestCase
 import io
 import sys
+import hello_world
 
-
-class TestHelloWord(TestCase):
-    
+class TestHelloWolrd(TestCase):
+    """TestCase Class for hello_world.py
+    """
     def test_prin_hello_world(self):
-        capturedOutput = io.StringIO()                                      # Create StringIO.
-        sys.stdout = capturedOutput                                         # Redirect stdout.
-        with open("hello_world.py") as f:                                   # Run python code.
-            exec(f.read())
-        sys.stdout = sys.__stdout__                                         # Reset redirect.
-        self.assertEqual('Hello World!\n', capturedOutput.getvalue())       # Assert.
+        """It should print 'Hello World!'"""
+        captured_output = io.StringIO()                                      # Create StringIO.
+        sys.stdout = captured_output                                         # Redirect stdout.
+        hello_world.print_msg()                                              # Print message.
+        sys.stdout = sys.__stdout__                                          # Reset redirect.
+        self.assertEqual('Hello World!\n', captured_output.getvalue())       # Assert.
